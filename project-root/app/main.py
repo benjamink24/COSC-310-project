ccc47f1ab47a4219188ef46959048388fc1a802d
khuranamanhar <khuranamanhar@gmail.com>
2026-09-24 21:54:07 -0700

    test pydantic and test services completed

--- project-root/app/main.py
+++ project-root/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 app.include_router(restaurant_router)
 #cd into app 
 # terminal comand to start hosting on port 8000:
-# uvicorn main:app --reload --host 127.0.0.1 --port 8000
+# uvicorn app.main:app --reload --host 127.0.0.1 --port 8000
 
 
 @app.get("/health")
